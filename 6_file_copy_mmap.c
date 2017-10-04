11d32c3ce3da529a1e2089d7e422aa66ae9556d4
Nutan Ghatge <nutanghatge@elear.solutions>
2017-10-04 17:25:08 +0530

    LSP exercise 6

--- 6_file_copy_mmap.c
+++ 6_file_copy_mmap.c
@@ -7,19 +7,19 @@
 #include <sys/mman.h>
 
 #define SOURCE_FILE "sam"
-#define DEST_FILE "tst"
+#define DEST_FILE "tls"
 
 int main() {
   int fdSrc, fdDst;
   char *srcAddr, *dstAddr;
   size_t size;
 
-  if ((fdSrc = (SOURCE_FILE, O_RDONLY)) < 0) {
+  if ((fdSrc = open(SOURCE_FILE, O_RDONLY)) < 0) {
     printf("Error Num >> %d\n", errno);
     perror(SOURCE_FILE);
   }
 
-  if ((fdDst = (DEST_FILE, O_WRONLY | O_CREAT)) < 0) {
+  if ((fdDst = open(DEST_FILE, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR|S_IWUSR)) < 0) {
     printf("Error Num >> %d\n", errno);
     perror(DEST_FILE);
   }
@@ -30,7 +30,11 @@ int main() {
     exit(1);
   }
 
+  if (ftruncate(fdDst,size) == -1) {
+    perror("truncate");
+  }
   if ((dstAddr = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fdDst,0)) == MAP_FAILED) {
+    printf("error no >> %d\n", errno);
     perror("mmap error");
     exit(2);
   }
