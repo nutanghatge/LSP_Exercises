04697529c22dda62688a460b451d5514b7118922
Nutan Ghatge <nutanghatge@elear.solutions>
2017-10-10 16:31:59 +0530

    LSP exercise 14

---
+++ 14_print_env.c
@@ -0,0 +1,72 @@
+/*===================================================================================*/
+/*************************************************************************************/
+/**
+ * @file      14_print_env.c
+ * @brief     Exercise on environmental variables
+ * @details   This program prints out the entire environment for the process 
+ * @see
+ * @author    Nutan Ghatge, nutanghatge@elear.solutions
+ * @copyright Copyright (c) 2017 Elear Solutions Tech Private Limited. All rights
+ *            reserved.
+ * @license   To any person (the "Recipient") obtaining a copy of this software and
+ *            associated documentation files (the "Software"):\n
+ *            All information contained in or disclosed by this software is
+ *            confidential and proprietary information of Elear Solutions Tech
+ *            Private Limited and all rights therein are expressly reserved.
+ *            By accepting this material the recipient agrees that this material and
+ *            the information contained therein is held in confidence and in trust
+ *            and will NOT be used, copied, modified, merged, published, distributed,
+ *            sublicensed, reproduced in whole or in part, nor its contents revealed
+ *            in any manner to others without the express written permission of
+ *            Elear Solutions Tech Private Limited.
+ */
+/*************************************************************************************/
+/*===================================================================================*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*************************************************************************************
+ *                          LOCAL MACROS                                             *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          LOCAL TYPEDEFS                                           *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          LOCAL PROTOTYPES                                         *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          GLOBAL VARIABLES                                         *
+ *************************************************************************************/
+extern char **environ;
+
+/*************************************************************************************
+ *                          LOCAL VARIABLES                                          *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          PRIVATE FUNCTIONS                                        *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          PUBLIC FUNCTIONS                                         *
+ *************************************************************************************/
+/*************************************************************************************
+ * Refer to the header file for a detailed description                               *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          MAIN                                                     *
+ *************************************************************************************/
+int main() {
+  char **p;
+  for (p = environ; *p != NULL; p++) {
+    if ((printf("%s\n", *p)) < 0) {
+      perror("printf error");
+      return EXIT_FAILURE;
+    }
+  }
+  return EXIT_SUCCESS;
+}
