bfaeb6545e6a14dd77cdd8d68ddba30ce71c7a44
Nutan Ghatge <nutanghatge@elear.solutions>
2017-10-11 14:55:35 +0530

    LSP exercise 23

---
+++ 23_shell_program.c
@@ -0,0 +1,78 @@
+/*===================================================================================*/
+/*************************************************************************************/
+/**
+ * @file      23_shell_progarm.c
+ * @brief     Exercise on execlp
+ * @details   A small shell program that has a command prompt “ashish> ” and uses
+ *            execlp() to execute any inputs provided by the user 
+ * @see
+ * @author    Nutan Ghatge, nutanghatge@elear.solutions
+ * @copyright Copyright (c) 2017 Elear Solutions Tech Private Limited. All rights
+ *            reserved.
+ * @license   To any person (the "Recipient") obtaining a copy of this software and
+ *            associated documentation files (the "Software"):\n
+ *            All information contained in or disclosed by this software is
+ *            confidential and proprietary information of Elear Solutions Tech
+ *            Private Limited and all rights therein are expressly reserved.
+ *            By accepting this material the recipient agrees that this material and
+ *            the information contained therein is held in confidence and in trust
+ *            and will NOT be used, copied, modified, merged, published, distributed,
+ *            sublicensed, reproduced in whole or in part, nor its contents revealed
+ *            in any manner to others without the express written permission of
+ *            Elear Solutions Tech Private Limited.
+ */
+/*************************************************************************************/
+/*===================================================================================*/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/*************************************************************************************
+ *                          LOCAL MACROS                                             *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          LOCAL TYPEDEFS                                           *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          LOCAL PROTOTYPES                                         *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          GLOBAL VARIABLES                                         *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          LOCAL VARIABLES                                          *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          PRIVATE FUNCTIONS                                        *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          PUBLIC FUNCTIONS                                         *
+ *************************************************************************************/
+/*************************************************************************************
+ * Refer to the header file for a detailed description                               *
+ *************************************************************************************/
+
+/*************************************************************************************
+ *                          MAIN                                                     *
+ *************************************************************************************/
+int main() {
+  char line[100];
+  while (printf("ashish>"), gets(line) != NULL) {
+    if (fork() == 0) {
+      if ((execlp(line, line, (char *)0)) == -1) {
+        perror("execlp failed");
+        printf("%s: not found\n", line);
+	}
+    } else  wait(0);
+  }
+  return EXIT_SUCCESS;
+}
